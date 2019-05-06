7db3c2bc7563b22a417fa231de28a7f85436a95d
Jakub Mroczkowski <jakub_mroczkowski@wp.pl>
2019-05-06 10:39:46 +0200

    Settings state improvements

--- GameProject/States/Settings.cs
+++ GameProject/States/Settings.cs
@@ -14,7 +14,10 @@ namespace GameProject.States
 	public class Settings : State
 	{
 		SpriteFont font;
-		public Settings(Game1 g, GraphicsDevice gd, ContentManager c) : base(g, gd, c)
+        List<string> inputKeybindStrings = new List<string>();
+        //Will represent max font width (from input keybinds strings)
+        Vector2 size = new Vector2(0, 0);
+        public Settings(Game1 g, GraphicsDevice gd, ContentManager c) : base(g, gd, c)
 		{
 			var background = content.Load<Texture2D>("Background");
 			var buttonTexture = content.Load<Texture2D>("Button");
@@ -25,31 +28,45 @@ namespace GameProject.States
             var c2 = content.Load<Texture2D>("CheckboxChecked");
 			font = content.Load<SpriteFont>("Font");
 
-			staticComponents = new List<Component>
-			{
-				new Sprite(background, g.Scale)
-				{
-					Position = new Vector2(0,0)
-				},
-				new Sprite(settingsTexture, g.Scale)
-				{
-					Position = new Vector2(1f/4f*game.Width, 0.03f*game.Height)
-				},
-				new Button(keyBorderTexture, buttonFont, g.Scale)
-				{
-					Position = new Vector2(0.25f*game.Width, 0.03f*game.Height)
-				},
-				new Button(buttonTexture, buttonFont, g.Scale)
-				{
-				Text = "Back",
-				Position = new Vector2(0.01f * g.Width, 0.9f * g.Height),
-				Click = Back
-				},
+            staticComponents = new List<Component>
+            {
+                new Sprite(background, g.Scale)
+                {
+                    Position = new Vector2(0,0)
+                },
+                new Sprite(settingsTexture, g.Scale)
+                {
+                    Position = new Vector2(1f/4f*game.Width, 0.03f*game.Height)
+                },
+                new Button(buttonTexture, buttonFont, g.Scale)
+                {
+                Text = "Back",
+                Position = new Vector2(0.01f * g.Width, 0.9f * g.Height),
+                Click = Back
+                },
                 new Checkbox(c1, c2, g.Scale)
                 {
                     Position = new Vector2(0.25f*game.Width, 0.6f*game.Height)
                 }
-			};
+            };
+
+            //Make keybinds strings
+            foreach (var s in game.Input.KeyBindings)
+            {
+                inputKeybindStrings.Add(s.Key.ToString());
+                Vector2 currentSize = font.MeasureString(s.Key.ToString());
+                size.X = Math.Max(size.X, currentSize.X);
+            }
+
+            //Make keybinds buttons (need to set correct Y still)
+            for (int i = 0; i < inputKeybindStrings.Count; i++)
+            {
+                staticComponents.Add(new Button(buttonTexture, font, g.Scale)
+                {
+                    Position = new Vector2(0.255f * game.Width + size.X, 0)
+                }
+                );
+            }
 		}
 
 		private void Back(object sender, EventArgs e)
@@ -62,9 +79,12 @@ namespace GameProject.States
 			spriteBatch.Begin();
 			foreach (var c in staticComponents)
 				c.Draw(gameTime, spriteBatch);
-			for(int i=0;i<game.Input.KeyBindings.Count;i++)
+
+            //Make it a list of strings in constructor
+			for(int i=0;i<inputKeybindStrings.Count;i++)
 			{
-				spriteBatch.DrawString(font, game.Input.KeyBindings.ElementAt(i).ToString(), new Vector2(game.Width/2, (i * 0.04f + 0.05f) * game.Height), Color.Black);
+                //Should be done with measure string (needs a fix later)
+				spriteBatch.DrawString(font, inputKeybindStrings.ElementAt(i), new Vector2(0.255f*game.Width, (i * 0.04f + 0.05f) * game.Height), Color.Black);
 			}
 			spriteBatch.End();
 		}
