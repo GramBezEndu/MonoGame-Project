aa8974b942f0bd0f52f17b1d7eecd685a0af7d5f
Jakub Mroczkowski <jakub_mroczkowski@wp.pl>
2019-07-05 13:07:33 +0200

    Updating upgrade cost

---
+++ GameProject/Inventory/DraggableBlacksmithSlot.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using GameProject.Sprites;
+using GameProject.Items;
+
+namespace GameProject.Inventory
+{
+	public class DraggableBlacksmithSlot : DraggableSlot
+	{
+		Blacksmith blacksmith;
+		public DraggableBlacksmithSlot(Blacksmith b, GraphicsDevice gd, Player p, Texture2D t, SpriteFont f, float scale) : base(gd, p, t, f, scale)
+		{
+			blacksmith = b;
+		}
+
+		//public override void DragAndDrop()
+		//{
+		//	//Item dragging
+		//	if (this.Draggable)
+		//	{
+		//		if (currentState.LeftButton == ButtonState.Released && previousState.LeftButton == ButtonState.Pressed)
+		//		{
+		//			//End dragging within the same slot
+		//			if (IsDragging)
+		//				IsDragging = false;
+		//			//End dragging within different slot
+		//			else if (player.InventoryManager.IsAlreadyDragging())
+		//			{
+		//				Slot slotDragging = player.InventoryManager.WhichSlotIsDragging();
+		//				//Swap normally
+		//				var item = slotDragging.Item;
+		//				slotDragging.Item = this.Item;
+		//				this.Item = item;
+		//				slotDragging.IsDragging = false;
+		//				////Update description (upgrade cost)
+		//				//if(Item is UpgradeableWithScroll)
+		//				//{
+		//				//	blacksmith.UpgradeCost = (Item as UpgradeableWithScroll).UpgradeCost;
+		//				//}
+		//				//else
+		//				//{
+		//				//	blacksmith.UpgradeCost = 0;
+		//				//}
+		//			}
+		//			//Try to start dragging
+		//			else if (!IsDragging)
+		//			{
+		//				//You can't start dragging two items -> extra check
+		//				if (player.InventoryManager.IsAlreadyDragging())
+		//					return;
+		//				else
+		//					IsDragging = true;
+		//			}
+		//		}
+		//	}
+		//}
+	}
+}
---
+++ GameProject/Inventory/DraggableBlacksmithsSlot.cs
@@ -0,0 +1,67 @@
+﻿using GameProject.Sprites;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using GameProject.States;
+using GameProject.Items;
+using GameProject.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject.Inventory
+{
+	public class DraggableBlacksmithsSlot : DraggableSlot
+	{
+		Blacksmith blacksmith;
+		public DraggableBlacksmithsSlot(Blacksmith b, GraphicsDevice gd, Player p, Texture2D t, SpriteFont f, float scale) : base(gd, p, t, f, scale)
+		{
+			blacksmith = b;
+		}
+
+		public override void DragAndDrop()
+		{
+			//Item dragging
+			if (this.Draggable)
+			{
+				if (currentState.LeftButton == ButtonState.Released && previousState.LeftButton == ButtonState.Pressed)
+				{
+					//End dragging within the same slot
+					if (IsDragging)
+						IsDragging = false;
+					//End dragging within different slot
+					else if (player.InventoryManager.IsAlreadyDragging())
+					{
+						Slot slotDragging = player.InventoryManager.WhichSlotIsDragging();
+						//Swap normally
+						var item = slotDragging.Item;
+						slotDragging.Item = this.Item;
+						this.Item = item;
+						slotDragging.IsDragging = false;
+						//Upgrade cost
+						if(this.Item is UpgradeableWithScroll)
+						{
+							var temp = (this.Item as UpgradeableWithScroll);
+							blacksmith.UpgradeCost = temp.UpgradeCost;
+						}
+						else
+						{
+							blacksmith.UpgradeCost = 0;
+						}
+					}
+					//Try to start dragging
+					else if (!IsDragging)
+					{
+						//You can't start dragging two items -> extra check
+						if (player.InventoryManager.IsAlreadyDragging())
+							return;
+						else
+							IsDragging = true;
+					}
+				}
+			}
+		}
+	}
+}
