70c5f2e73dbec0786a6175049e382413fb2b46f4
Jakub Mroczkowski <jakub_mroczkowski@wp.pl>
2019-07-11 20:34:23 +0200

    Slider can be dragged now

--- GameProject/Controls/Slider.cs
+++ GameProject/Controls/Slider.cs
@@ -16,17 +16,38 @@ namespace GameProject.Controls
 		Text HowMuchBarFilled;
 		float maxValue = 1f;
 		float minValue = 0f;
-		public float CurrentValue {get; set;} = 0.5f;
+		public float CurrentValue
+		{
+			get => _currentValue;
+			set
+			{
+				_currentValue = value;
+				SetPercentage();
+				HowMuchBarFilled.Message = GetPercentage();
+			}
+		}
+		private float _currentValue;
 
-		string Percentage;
+		private string percentage;
+		public string GetPercentage()
+		{
+			return percentage;
+		}
+
+		public void SetPercentage()
+		{
+			percentage = string.Format("{0}%", (CurrentValue / Math.Abs(maxValue - minValue)) * 100);
+		}
+
+
 		SpriteFont Font;
 		Input input;
 
-		public Slider(Input i,Texture2D sliderBorder, Texture2D sliderFilled, SpriteFont f, float scale) : base(sliderBorder, scale)
+		public Slider(Input i, Texture2D sliderBorder, Texture2D sliderFilled, SpriteFont f, float scale) : base(sliderBorder, scale)
 		{
 			SliderFilled = sliderFilled;
-			Percentage = string.Format("{0}%", (CurrentValue / Math.Abs(maxValue - minValue)) * 100);
-			HowMuchBarFilled = new Text(f, Percentage);
+			SetPercentage();
+			HowMuchBarFilled = new Text(f, percentage);
 			Font = f;
 			input = i;
 		}
@@ -47,7 +68,7 @@ namespace GameProject.Controls
 					SpriteEffects.None,
 					0f);
 				//Writes % how much slider is filled
-				HowMuchBarFilled.Position = new Vector2(this.Position.X + this.Width / 2 - Font.MeasureString(Percentage).X/2, this.Position.Y);
+				HowMuchBarFilled.Position = new Vector2(this.Position.X + this.Width / 2 - Font.MeasureString(GetPercentage()).X / 2, this.Position.Y);
 				HowMuchBarFilled.Draw(gameTime, spriteBatch);
 			}
 		}
@@ -57,10 +78,14 @@ namespace GameProject.Controls
 			var mouseRectangle = new Rectangle(input.CurrentMouseState.X, input.CurrentMouseState.Y, 1, 1);
 			if (mouseRectangle.Intersects(Rectangle))
 			{
-				//User clicked somewhere on slider
-				if(input.CurrentMouseState.LeftButton == ButtonState.Released && input.PreviousMouseState.LeftButton == ButtonState.Pressed)
+				Vector2 mousePos = new Vector2(input.CurrentMouseState.X, input.CurrentMouseState.Y);
+				//Left mouse button is pressed
+				if (input.CurrentMouseState.LeftButton == ButtonState.Pressed)
 				{
+					Vector2 positionRelated = mousePos - this.Position;
+					int width = (int)positionRelated.X;
 
+					CurrentValue = (float)Math.Round(((float)width / this.Width), 2);
 				}
 			}
 		}
